7bbe179457d6e1c639220454eea89a8df5e81149
Julianouefs <julianouefs@gmail.com>
2025-07-19 06:01:51 -0300

    Update app_biblioteca_web.py

--- app_biblioteca_web.py
+++ app_biblioteca_web.py
@@ -125,7 +125,7 @@ with st.expander("🔐 Administrador"):
                     st.success("Login realizado com sucesso.")
                     st.session_state.modo_admin = True
                     st.session_state.login_time = datetime.now()
-                    st.experimental_rerun()
+                    st.rerun()
                 else:
                     st.error("Usuário ou senha incorretos.")
     else:
@@ -139,7 +139,7 @@ with st.expander("🔐 Administrador"):
                 else:
                     df_novo.to_excel(ARQUIVO_PLANILHA, index=False)
                     st.success("Planilha atualizada com sucesso!")
-                    st.experimental_rerun()
+                    st.rerun()
             except Exception as e:
                 st.error(f"Erro ao processar o arquivo: {e}")
 
@@ -196,7 +196,7 @@ with st.expander("🔐 Administrador"):
                             worksheet.append_row(nova_linha)
                             # Atualiza dados_emprestimos e worksheet para refletir a mudança
                             st.success(f"✅ Empréstimo de '{nome_livro}' registrado com sucesso.")
-                            st.experimental_rerun()
+                            st.rerun()
                         except Exception as e:
                             st.error(f"Erro ao registrar o empréstimo: {e}")
 
@@ -241,7 +241,7 @@ with st.expander("🔐 Administrador"):
                         worksheet.update_cell(linha_para_atualizar, worksheet.find("Data de devolução").col, data_hoje)
                         worksheet.update_cell(linha_para_atualizar, worksheet.find("Situação").col, "Devolvido")
                         st.success(f"Devolução registrada para '{linha_devolucao['Título do Livro']}' com data {data_hoje}.")
-                        st.experimental_rerun()
+                        st.rerun()
 
         except Exception as e:
             st.error(f"Erro ao carregar dados para devolução: {e}")
